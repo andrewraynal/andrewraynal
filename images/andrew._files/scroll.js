ed5faf5aa1bb7ad03fb9ef32ee27e37c0f72a266
andrew <andrewraynal@me.com>
2014-07-30 14:30:02 -0400

    update..

--- images/andrew._files/scroll.js
+++ images/andrew._files/scroll.js
@@ -9,24 +9,24 @@ $(document).ready(function(){
 			var here = getPos();
 			if (here <= 10) {
 				bgImage.css('background-image', 'url("images/livemusic.jpg")');
-			} else if (here <= 75) {
+			} else if (here <= 100) {
 				bgImage.css('-webkit-filter', 'brightness(150%)');
 			}
-			else if (here <= 150) {
+			else if (here <= 200) {
 				bgImage.css('-webkit-filter', 'brightness(200%)');
 			}
 				});
 
 		var bgProjectOne = $('.red');
-		var bgProjectTwo = $('.green');
-		var bgProjectThree = $('.blue');
+		// var bgProjectTwo = $('.green');
+		// var bgProjectThree = $('.blue');
 		$(window).scroll(function() {
 			var here = getPos();
 			console.log(here);
-			if (here >= 1300) {
-				bgProjectOne.css('-webkit-filter', 'brightness(300%)');
-				bgProjectTwo.css('-webkit-filter', 'brightness(300%)');
-				bgProjectThree.css('-webkit-filter', 'brightness(300%)');
+			if (here <= 1300) {
+				bgProjectOne.css('-webkit-filter', 'brightness(200%)');
+		// 		bgProjectTwo.css('-webkit-filter', 'brightness(200%)');
+		// 		bgProjectThree.css('-webkit-filter', 'brightness(200%)');
 			
 			}
 		}); 
